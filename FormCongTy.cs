c8a7ac7787e3e7f7e088a60ca0fbe4f932ad0af7
hongvuong2kk3-hash <hongvuong2kk3@gmail.com>
2025-09-14 16:45:55 +0700

    Add files via upload

---
+++ FormCongTy.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+using BusinessLayer;
+
+namespace Qly_NVien
+{
+    public partial class FormCongTy : Form
+    {
+        public FormCongTy()
+        {
+            InitializeComponent();
+        }
+
+        //BIẾN
+        CONGTY_bs _congty;
+        bool _them;
+        int _idct;
+
+        private void FormCongTy_Load(object sender, EventArgs e)
+        {
+            //CHIỀU CAO DÒNG
+            gvDanhSach.RowHeight = 30;
+            gvDanhSach.ColumnPanelRowHeight = 40;
+            //TIÊU ĐỀ CỘT
+            gvDanhSach.Appearance.HeaderPanel.Font = new Font("Times New Roman", 15F, FontStyle.Bold);
+            gvDanhSach.Appearance.HeaderPanel.Options.UseFont = true;
+            //Ô DỮ LIỆU
+            gvDanhSach.Appearance.Row.Font = new Font("Times New Roman", 15F);
+            gvDanhSach.Appearance.Row.Options.UseFont = true;
+            //TỤ ĐỘNG CO DÃN THEO NỘI DUNG
+            gvDanhSach.OptionsView.RowAutoHeight = true;
+
+            //THAO TÁC
+            _them = false;
+            _congty = new CONGTY_bs();
+            showHide(true);
+            loadData();
+            splitContainerControl1.PanelVisibility = DevExpress.XtraEditors.SplitPanelVisibility.Panel2;    //CHỈ HIỆN THỊ PANEL 2
+        }
+
+        //ẨN NÚT LƯU VÀ HỦY KHI THAO TÁC
+        void showHide(bool kt)
+        {
+            btnLuu.Enabled = !kt;
+            btnHuy.Enabled = !kt;
+            btnThem.Enabled = kt;
+            btnSua.Enabled = kt;
+            btnXoa.Enabled = kt;
+            btDong.Enabled = kt;
+            btIn.Enabled = kt;
+            textEditTen.Enabled = !kt;
+            textEditSoDienThoai.Enabled = !kt;
+            textEditEmail.Enabled = !kt;
+            textEditDiaChi.Enabled = !kt;
+
+        }
+
+        void loadData()
+        {
+            gcDanhSach.DataSource = _congty.getList();
+            gvDanhSach.OptionsBehavior.Editable = false;
+        }
+
+        private void btnThem_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            showHide(false);
+            _them = true;
+            textEditTen.Text = string.Empty;
+            textEditSoDienThoai.Text = string.Empty;
+            textEditEmail.Text = string.Empty;
+            textEditDiaChi.Text = string.Empty;
+            splitContainerControl1.PanelVisibility = DevExpress.XtraEditors.SplitPanelVisibility.Both; //HIỆN THỊ CẢ 2 PANEL
+        }
+
+        private void btnSua_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = false;
+            showHide(false);
+            splitContainerControl1.PanelVisibility = DevExpress.XtraEditors.SplitPanelVisibility.Both; //HIỆN THỊ CẢ 2 PANEL
+        }
+
+        private void btnXoa_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn xóa không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                _congty.Delete(_idct);
+                loadData();
+            }
+        }
+
+        private void btnLuu_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (textEditTen.Text == "" || textEditDiaChi.Text == "" || textEditEmail.Text == "" || textEditSoDienThoai.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                saveData();
+                loadData();
+                _them = false;
+                showHide(true);
+                splitContainerControl1.PanelVisibility = DevExpress.XtraEditors.SplitPanelVisibility.Panel2;    //CHỈ HIỆN THỊ PANEL 2
+            }
+
+        }
+
+        private void btnHuy_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = false;
+            showHide(true);
+            splitContainerControl1.PanelVisibility = DevExpress.XtraEditors.SplitPanelVisibility.Panel2;    //CHỈ HIỆN THỊ PANEL 2
+        }
+
+        private void btIn_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btDong_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            this.Close();
+        }
+
+        //HÀM LƯU DL
+        void saveData()
+        {
+            if (_them)
+            {
+                CONGTY ct = new CONGTY();
+                ct.TENCTY = textEditTen.Text;
+                ct.SĐT = textEditSoDienThoai.Text;
+                ct.EMAIL = textEditEmail.Text;
+                ct.DIACHI = textEditDiaChi.Text;
+                _congty.Add(ct);
+            }
+            else
+            {
+                var ct = _congty.getItem(_idct);
+                ct.TENCTY = textEditTen.Text;
+                ct.SĐT = textEditSoDienThoai.Text;
+                ct.EMAIL = textEditEmail.Text;
+                ct.DIACHI = textEditDiaChi.Text;
+                _congty.Update(ct);
+            }
+        }
+
+        private void gvDanhSach_Click(object sender, EventArgs e)
+        {
+            if(gvDanhSach.RowCount > 0)
+            {
+                _idct = int.Parse(gvDanhSach.GetFocusedRowCellValue("ID_CTY").ToString());
+                textEditTen.Text = gvDanhSach.GetFocusedRowCellValue("TENCTY").ToString();
+                textEditSoDienThoai.Text = gvDanhSach.GetFocusedRowCellValue("SĐT").ToString();
+                textEditEmail.Text = gvDanhSach.GetFocusedRowCellValue("EMAIL").ToString();
+                textEditDiaChi.Text = gvDanhSach.GetFocusedRowCellValue("DIACHI").ToString();
+            }
+        }
+    }
+}
